00b9bea4850f40d48f002ff8c88d3bd9b11accb8
iskeym <95422342+iskeym@users.noreply.github.com>
2022-02-17 20:33:41 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def link(driver, links, page, u):
+    url = (f'{u}&p={page}')
+    driver.get(url)
+
+    items = driver.find_elements(By.CLASS_NAME, "js-catalog-item-enum")
+    for item in items:
+        link = item.find_element(By.CLASS_NAME, "title-root_maxHeight-X6PsH").get_attribute('href')
+        links.append(link)
+
+def info(driver, total, links):
+    for link in links:
+        driver.get(link)
+
+        x = []
+        x.append({
+            'title': driver.find_element(By.XPATH, "//span[@class='title-info-title-text']").text,
+            'price': driver.find_element(By.XPATH, "//span[@class='js-item-price']").text,
+            'address': driver.find_element(By.XPATH, "//span[@class='item-address__string']").text,
+            'name': driver.find_element(By.XPATH, "//div[@class='seller-info-name js-seller-info-name']//a").text,
+            'articul': driver.find_element(By.XPATH, "//span[@data-marker='item-view/item-id']").text,
+            'link': link
+        })
+
+        print(x)
+        total.extend(x)
+
+def save(total):
+    with open('parser.csv', 'w', newline='') as ex:
+        writer = csv.writer(ex, delimiter=';')
+        writer.writerow(['название', 'цена', 'адрес', 'имя', 'номер товара', 'ссылка'])
+        for dict in total:
+            writer.writerow([dict['title'], dict['price'], dict['address'], dict['name'], dict['articul'], dict['link']])
+
+def parser():
+    url = input('введите ссылку авито: ')
+    pages = int(input('сколько страниц: '))
+    driver = webdriver.Chrome()
+
+    links = []
+    for page in range(1, pages + 1):
+        link(driver, links, page, url)
+
+    print(links)
+
+    total = []
+    info(driver, total, links)
+
+    print(total)
+
+    save(total)
+    os.startfile('parser.csv')
+
+    driver.close()
+
+parser()
